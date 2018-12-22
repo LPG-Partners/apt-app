3bd927084b9002aa2c17247794c90658c6aa03bd
Isai Lopez <ilopezro@ucsc.edu>
2018-12-21 18:35:40 -0800

    Added database folder and schema.sql

---
+++ database/schema.sql
@@ -0,0 +1,32 @@
+/* this will delete and crete apartmentApp database*/
+drop database if exists apartmentApp;
+create database apartmentApp;
+
+/* this will enter database*/
+use apartmentApp;
+
+/* this will create user database*/
+create table users(
+  userID integer primary key,
+  email text,
+  userName text,
+  password text,
+  securityQ1 text,
+  securityQ2 text,
+  securityQ3 text,
+);
+
+/* this will create group database*/
+create table groups(
+  groupID integer primary key AUTOINCREMENT,
+  leaderID integer,
+  foreign key (leaderID) references users(userID)
+);
+
+/* this will create group_user database*/
+create table groupUsers(
+  groupID integer,
+  userID integer,
+  foreign key (groupID) references users(userID),
+  foreign key (userID) references users(userID)
+);
