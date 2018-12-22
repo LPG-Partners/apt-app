522658d400ba7f9fbab1d05ad1a8ac8f3c2df221
Isai Lopez <ilopezro@ucsc.edu>
2018-12-21 22:36:41 -0800

    updated schedma.sql; changed naming conventions

--- database/schema.sql
+++ database/schema.sql
@@ -1,32 +1,36 @@
 /* this will delete and crete apartmentApp database*/
 drop database if exists apartmentApp;
 create database apartmentApp;
- 
+
 
 /* this will enter database*/
 use apartmentApp;
 
 /* this will create user database*/
 create table users(
-  userID integer primary key AUTOINCREMENT,
-  email text NOT NULL,
-  password text NOT NULL,
+  user_id integer primary key auto_increment,
+  email text not null,
+  password text not null,
   securityQ1 text,
   securityQ2 text,
-  securityQ3 text,
+  securityQ3 text
 );
 
 /* this will create group database*/
-create table groups(
-  groupID integer primary key AUTOINCREMENT,
-  leaderID integer,
-  foreign key (leaderID) references users(userID)
+
+CREATE TABLE aptGroups (
+   group_id int not null auto_increment,
+   leader_id int not null,
+   primary key (group_id),
+   foreign key (leader_id) references users(user_id)
 );
 
 /* this will create group_user database*/
-create table groupUsers(
-  groupID integer,
-  userID integer,
-  foreign key (groupID) references users(userID),
-  foreign key (userID) references users(userID)
+create table groupUserRelationship(
+  group_user_relationship_id int not null auto_increment,
+  group_id int,
+  user_id int,
+  primary key (group_user_relationship_id),
+  foreign key (group_id) references aptGroups(group_id),
+  foreign key (user_id) references users(user_id)
 );
