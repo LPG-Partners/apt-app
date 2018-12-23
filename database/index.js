2b7aaf819728d0847e36ac1b5f51e688d226c596
Isai Lopez <ilopezro@ucsc.edu>
2018-12-22 17:19:32 -0800

    added methods.js file, deleted connection end from index.js, deleted test file

--- database/index.js
+++ database/index.js
@@ -22,5 +22,3 @@ exports.connection = connection;
 // Create a database connection and export it from this file.
 // You will need to connect with the user "root", no password,
 // and to the database "apartmentApp".
-
-connection.end(); 
