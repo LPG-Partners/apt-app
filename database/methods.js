b13b5c7361bbd0403e19904b58ffd68ce0711d3d
Isai Lopez <ilopezro@ucsc.edu>
2018-12-23 15:40:39 -0800

    created logInUser method that succesfully logs users in

--- database/methods.js
+++ database/methods.js
@@ -26,7 +26,25 @@ const signUpUser = (firstName, lastName, email, password, phoneNumber, recoveryE
   });
 };
 
-// signUpUser('greg', 'pereira', 'fakemail1@gmail.com', 'p4ssw0rd', '4152502472', 'fakeemail2@gmail.com')
+//this will allow for user to log in
+const logUserIn = (email, password) => {
+  let queryString = `Select * from users u where u.user_email = '${email}' AND u.user_password = '${password}';`;
+  console.log(queryString);
+  connection.query(queryString, (error, results, fields) => {
+    if (error) throw error;
+    else if(results.length === 0){
+      console.log("unsuccesful log in");
+    }else{
+      console.log("succesful log in");
+    }
+  })
+};
+
+// signUpUser('greg', 'pereira', 'fakemail1@gmail.com', 'p4ssw0rd', '4152502472', 'fakeemail2@gmail.com');
+
+logUserIn('user@example.com', 'password');
+
+
 
 const updateUserPass = (email, oldPassword, newPassword) => {
   queryString = `UPDATE users u SET u.user_password = '${newPassword}' WHERE u.user_email = '${email}' AND u.user_password = '${oldPassword}';`;
@@ -37,18 +55,18 @@ const updateUserPass = (email, oldPassword, newPassword) => {
   })
 };
 
-updateUserPass('fakemail1@gmail.com', 'p4ssw0rd', 'NeWp4sSw0rD');
-// posts 
-  // add user 
+// updateUserPass('fakemail1@gmail.com', 'p4ssw0rd', 'NeWp4sSw0rD');
+// posts
+  // add user
 
 
 // reads
 
 // deletes
-  // delete a user 
+  // delete a user
 
 // updates
   // update email with verification
-  // update a phone number 
+  // update a phone number
 
   //update password by username and password
